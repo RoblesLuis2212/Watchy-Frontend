9e14cbfb4d22d4e1c8b1f723dfd07bcc666d3a31
Luis Robles <luisgeremiasrobles@gmail.com>
2026-02-04 12:27:05 -0300

    Adapte las imagenes para los distintos tamaños de dispositivos

--- src/components/pages/Banner/Banner.jsx
+++ src/components/pages/Banner/Banner.jsx
@@ -8,12 +8,12 @@ import "./Banner.css";
 const Banner = () => {
     return (
         <section className='banner-section'>
-            <Carousel>
+            <Carousel controls={false} indicators={false} interval={4000}>
                 <Carousel.Item>
                     <img src={elConjuro} className='d-block w-100 banner-image image-conjuro' alt="Imagen de portada de pelicula el conjuro" />
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img src={breakingBad} className='d-block w-100 banner-image image-breakingbad' alt="Imagen de portada de pelicula volver al futuro 3" />
+                    <img src={breakingBad} className='d-block w-100 banner-image' alt="Imagen de portada de pelicula volver al futuro 3" />
                 </Carousel.Item>
                 <Carousel.Item>
                     <img src={RayayelDragon} className='d-block w-100 banner-image' alt="Imagen de portada de pelicula Raya y el ultimo dragon" />
