39c7020a9c12030d5a1379c95f0d7381b21a3a27
Luis Robles <luisgeremiasrobles@gmail.com>
2026-02-04 20:30:03 -0300

    Agregue las columas del componente footer

--- src/components/pages/Footer/Footer.jsx
+++ src/components/pages/Footer/Footer.jsx
@@ -18,8 +18,22 @@ const Footer = () => {
                     <Link>Mi Perfil</Link>
                     <Link>Premium</Link>
                 </div>
-                <div className="col-12 col-md-6 col-lg-3 border border-light">1234</div>
-                <div className="col-12 col-md-6 col-lg-3 border border-light">1234</div>
+                <div className="col-12 col-md-6 col-lg-3 d-flex flex-column border border-light">
+                    <h5>Ayuda</h5>
+                    <Link>Politica de Privacidad</Link>
+                    <Link>Contacto</Link>
+                    <Link>Soporte Tecnico</Link>
+                    <Link>Quienes Somos</Link>
+                </div>
+                <div className="col-12 col-md-6 col-lg-3 border border-light">
+                    <h5>Siguenos en nuestras redes</h5>
+                    <div className="d-flex ms-5">
+                        <i className="bi bi-facebook me-3 fs-2"></i>
+                        <i className="bi bi-instagram me-3 fs-2"></i>
+                        <i className="bi bi-twitter-x me-3 fs-2"></i>
+                        <i className="bi bi-tiktok me-3 fs-2"></i>
+                    </div>
+                </div>
             </div>
         </footer>
     );
