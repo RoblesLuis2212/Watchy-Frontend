cbd17e6af35551fe62f5b0543c39edfe3394869c
Luis Robles <luisgeremiasrobles@gmail.com>
2026-02-04 21:11:52 -0300

    Agregue un undeline a los links del footer

--- src/components/pages/Footer/Footer.jsx
+++ src/components/pages/Footer/Footer.jsx
@@ -4,30 +4,30 @@ const Footer = () => {
     return (
         <footer className='container-fluid'>
             <div className="row">
-                <div className="col-12 col-md-6 col-lg-3 d-flex flex-column border border-light">
-                    <h5>Watchy</h5>
-                    <Link>Inicio</Link>
-                    <Link>Peliculas</Link>
-                    <Link>Series</Link>
-                    <Link>Novedades</Link>
+                <div className="col-12 col-md-4 col-lg-3 d-flex flex-column align-items-center align-items-md-start">
+                    <h5 className="mt-2">Watchy</h5>
+                    <Link className="text-secondary-emphasis link-underline-secondary link-underline-opacity-50">Inicio</Link>
+                    <Link className="text-secondary-emphasis link-underline-secondary link-underline-opacity-50">Peliculas</Link>
+                    <Link className="text-secondary-emphasis link-underline-secondary link-underline-opacity-50">Series</Link>
+                    <Link className="text-secondary-emphasis link-underline-secondary link-underline-opacity-50">Novedades</Link>
                 </div>
-                <div className="col-12 col-md-6 col-lg-3 d-flex flex-column border border-light">
-                    <h5>Tu Cuenta</h5>
-                    <Link>Iniciar Sesion</Link>
-                    <Link>Registrarse</Link>
-                    <Link>Mi Perfil</Link>
-                    <Link>Premium</Link>
+                <div className="col-12 col-md-4 col-lg-3 d-flex flex-column align-items-center align-items-md-start">
+                    <h5 className="mt-2">Tu Cuenta</h5>
+                    <Link className="text-secondary-emphasis link-underline-secondary link-underline-opacity-50">Iniciar Sesion</Link>
+                    <Link className="text-secondary-emphasis link-underline-secondary link-underline-opacity-50">Registrarse</Link>
+                    <Link className="text-secondary-emphasis link-underline-secondary link-underline-opacity-50">Mi Perfil</Link>
+                    <Link className="text-secondary-emphasis link-underline-secondary link-underline-opacity-50">Premium</Link>
                 </div>
-                <div className="col-12 col-md-6 col-lg-3 d-flex flex-column border border-light">
-                    <h5>Ayuda</h5>
-                    <Link>Politica de Privacidad</Link>
-                    <Link>Contacto</Link>
-                    <Link>Soporte Tecnico</Link>
-                    <Link>Quienes Somos</Link>
+                <div className="col-12 col-md-4 col-lg-3 d-flex flex-column align-items-center align-items-md-start">
+                    <h5 className="mt-2">Ayuda</h5>
+                    <Link className="text-secondary-emphasis link-underline-secondary link-underline-opacity-50">Politica de Privacidad</Link>
+                    <Link className="text-secondary-emphasis link-underline-secondary link-underline-opacity-50">Contacto</Link>
+                    <Link className="text-secondary-emphasis link-underline-secondary link-underline-opacity-50">Soporte Tecnico</Link>
+                    <Link className="text-secondary-emphasis link-underline-secondary link-underline-opacity-50">Quienes Somos</Link>
                 </div>
-                <div className="col-12 col-md-6 col-lg-3 border border-light">
-                    <h5>Siguenos en nuestras redes</h5>
-                    <div className="d-flex ms-5">
+                <div className="col-12 col-md-12 col-lg-3">
+                    <h5 className="mt-2 d-none d-lg-block text-center">Siguenos en nuestras redes</h5>
+                    <div className="d-flex justify-content-center ms-4">
                         <i className="bi bi-facebook me-3 fs-2"></i>
                         <i className="bi bi-instagram me-3 fs-2"></i>
                         <i className="bi bi-twitter-x me-3 fs-2"></i>
