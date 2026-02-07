5417dacf60dd5b728d00598ac4319ca04bcc3335
Luis Robles <luisgeremiasrobles@gmail.com>
2026-02-07 20:10:05 -0300

    Agregue la fuente de las secciones del inicio a la hoja de estilos principal

---
+++ src/components/pages/SeccionSeries/SeccionSeries.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SeccionSeries = () => {
+    return (
+        <section className='container-fluid py-3'>
+            <h2 className='text-center titulo-peliculas'>Series</h2>
+            <div className="row">
+                <div className="col-12 col-md-4 col-lg-3 border border-light">1234</div>
+                <div className="col-12 col-md-4 col-lg-3 border border-light">1234</div>
+                <div className="col-12 col-md-4 col-lg-3 border border-light">1234</div>
+                <div className="col-12 col-md-4 col-lg-3 border border-light">1234</div>
+            </div>
+        </section>
+    );
+};
+
+export default SeccionSeries;
