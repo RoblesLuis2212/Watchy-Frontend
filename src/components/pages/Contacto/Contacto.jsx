12b185932a67e1b106334b55102daac284aad24c
Luis Robles <luisgeremiasrobles@gmail.com>
2026-02-12 10:34:57 -0300

    Agregue un componente contacto y una ruta que nos lleva hacia ahi

---
+++ src/components/pages/Contacto/Contacto.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contacto = () => {
+    return (
+        <section className='container'>
+            <h2>Pagina de contacto</h2>
+        </section>
+    );
+};
+
+export default Contacto;
