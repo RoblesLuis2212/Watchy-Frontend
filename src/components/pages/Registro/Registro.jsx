a2feddd9cf4ab904e991ff4a3a9508987068146a
Luis Robles <luisgeremiasrobles@gmail.com>
2026-02-12 11:44:36 -0300

    Agregue una animacion al formulario y un fondo

--- src/components/pages/Registro/Registro.jsx
+++ src/components/pages/Registro/Registro.jsx
@@ -1,8 +1,44 @@
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Registro = () => {
     return (
-        <h1>Pagina de registro</h1>
+        <section className="container border border-emphasis rounded-4 py-3 px-3 mt-5 mb-3 container-contacto fade-in">
+            <h2 className='text-center'>Crear Cuenta</h2>
+            <Form>
+                <Form.Group className="mb-3">
+                    <Form.Label>Nombre de usuario</Form.Label>
+                    <Form.Control type="text" placeholder="ej: juanperez21" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Correo Electronico</Form.Label>
+                    <Form.Control type="text" placeholder="Ej: juanperez@gmail.com" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Contraseña</Form.Label>
+                    <Form.Control type="password" placeholder="minimo 8 caracteres" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Confirmar contraseña</Form.Label>
+                    <Form.Control type="password" placeholder='repetir contraseña' />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <button className='btn btn-categorias w-100' type="submit">
+                    Crear Cuenta
+                </button>
+            </Form>
+        </section>
     );
 };
 
