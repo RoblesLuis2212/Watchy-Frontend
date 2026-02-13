7b2447b11b0faea5e7ebcbd02835da47a0a06cee
Luis Robles <luisgeremiasrobles@gmail.com>
2026-02-13 10:46:43 -0300

    Agregue un modal que contendra el formulario de login

--- src/components/NavbarPage.jsx
+++ src/components/NavbarPage.jsx
@@ -5,37 +5,42 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import Form from 'react-bootstrap/Form';
 import "../index.css"
 import { Link } from 'react-router-dom';
+import { Modal } from 'bootstrap';
+import ModalLogin from './pages/Login/ModalLogin';
 
 const NavbarPage = () => {
     return (
-        <Navbar expand="lg" className="navbar-watchy">
-            <Container fluid>
-                <Navbar.Brand className='ms-2'>Watchy<i className="bi bi-camera-reels-fill ms-2"></i></Navbar.Brand>
-                <Form className='mx-auto d-md-none' style={{ width: "40%" }}>
-                    <InputGroup>
-                        <Form.Control
-                            placeholder="buscar pelicula o serie"
-                        />
-                    </InputGroup>
-                </Form>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse>
-                    {/* Buscador */}
-                    <Form className='mx-auto d-none d-md-block' style={{ width: "40%" }}>
+        <>
+            <Navbar expand="lg" className="navbar-watchy">
+                <Container fluid>
+                    <Navbar.Brand className='ms-2'>Watchy<i className="bi bi-camera-reels-fill ms-2"></i></Navbar.Brand>
+                    <Form className='mx-auto d-md-none' style={{ width: "40%" }}>
                         <InputGroup>
                             <Form.Control
                                 placeholder="buscar pelicula o serie"
                             />
                         </InputGroup>
                     </Form>
-                    <Nav className='d-flex gap-3 me-5'>
-                        <Nav.Link as={Link} to={"/registro"} className='text-white'>Crear Cuenta</Nav.Link>
-                        <Nav.Link className='text-white'>Iniciar Sesion</Nav.Link>
-                        <Nav.Link className='text-white'>Mi Lista</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse>
+                        {/* Buscador */}
+                        <Form className='mx-auto d-none d-md-block' style={{ width: "40%" }}>
+                            <InputGroup>
+                                <Form.Control
+                                    placeholder="buscar pelicula o serie"
+                                />
+                            </InputGroup>
+                        </Form>
+                        <Nav className='d-flex gap-3 me-5'>
+                            <Nav.Link as={Link} to={"/registro"} className='text-white'>Crear Cuenta</Nav.Link>
+                            <Nav.Link className='text-white'>Iniciar Sesion</Nav.Link>
+                            <Nav.Link className='text-white'>Mi Lista</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <ModalLogin></ModalLogin>
+        </>
     );
 };
 
